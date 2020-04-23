db7c240a781a4a5c861e7aad9ba1edf1acb27d14
Will Fulton <wbfulton@me.com>
2020-04-23 13:21:51 -0700

    visualizer works with no bugs

--- src/Algorithms/bubbleSort.js
+++ src/Algorithms/bubbleSort.js
@@ -1,5 +1,9 @@
 import { swapBars } from '../utils/utils.js';
 
+// press button and move index and compare
+// only if we already performed action on current compare
+// we want to compare first, then operate
+
 function bubbleSort(
     array,
     index,
@@ -10,32 +14,35 @@ function bubbleSort(
     currCompare
 ) {
     // THIS DOES BUBBLE SORT STEP WISE
-    console.log(index + " " + (array.length - 2))
+
+    // reset index at end of array
     if (index === array.length - 2) {
         setIndex(0);
+        // resets entirely if we do not
+        if(!(array[index] > array[index + 1])) {
+            setCompare(0, 1);
+        }
     }
-    // press button and move index and compare
-    // only if we already performed action on current compare
-    // we want to compare first, then operate
+
     const lookedAt = currCompare[0] === index && currCompare[1] === index + 1;
     
+    // clear swap
+    setSwap(-1, -1);
 
     if (lookedAt) {
-        //swap
+        // swap or move on
         if (array[index] > array[index + 1]) {
             const newArray = swapBars(array, index, index + 1);
             setSwap(index, index + 1);
             setArray(newArray);
-        } else { // move on
+        } else if (index !== array.length - 2) {
+            // move on
             setIndex(index + 1);
-            // reset swap
-            setSwap(-1, -1);
-            // we have check up above so this works
             setCompare(index + 1, index + 2);
         }
     } else {
+        // look at
         setCompare(index, index + 1);
-        setSwap(-1, -1);
     }
 }
 
